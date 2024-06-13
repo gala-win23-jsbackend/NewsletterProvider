d0357c07ba00e83f9622de8f007edcb8d09f6cf5
galinamiteva <galina.miteva@utb.ecutbildning.se>
2024-06-13 02:05:21 +0200

    satte false i Newsletters Entity

--- Data/Entities/SubscribeEntity.cs
+++ Data/Entities/SubscribeEntity.cs
@@ -9,10 +9,10 @@ public class SubscribeEntity
     [Key]
     public string Email { get; set; } = null!;
     public string? PreferredEmail { get; set; }
-    public bool Circle1 { get; set; }
-    public bool Circle2 { get; set; }
-    public bool Circle3 { get; set; }
-    public bool Circle4 { get; set; }
-    public bool Circle5 { get; set; }
-    public bool Circle6 { get; set; }
+    public bool Circle1 { get; set; } = false;
+    public bool Circle2 { get; set; } = false;
+    public bool Circle3 { get; set; } = false;
+    public bool Circle4 { get; set; } = false;
+    public bool Circle5 { get; set; } = false;
+    public bool Circle6 { get; set; } = false;
 }
